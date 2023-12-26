a006d4b8f0ff622d4cd164c26f4c433f8cc253d8
Norbi5930 <bnorbert0925@gmail.com>
2023-12-26 18:26:16 +0100

    Béta roulette rendszer

--- slot/static/JS/roulette.js
+++ slot/static/JS/roulette.js
@@ -1,6 +1,10 @@
 var oldCoin;
 var selected_coin;
-var numbers =[];
+var numbers =[
+  {"money": 100,
+    "selected": 16  
+  }
+];
 function coin_active(coinID) {
   selected_coin = document.getElementById(coinID);
   
@@ -22,13 +26,61 @@ function coin_active(coinID) {
 };
 
 
-const spin_button = document.getElementById("spinbutton").addEventListener("click", function() { spin_wheel() });
+
+var time = 10;
+var timer;
+var displayTimer = document.getElementById("timer")
+var wheel_spinning;
+
+
+var storageTime = localStorage.getItem("timerTime");
+if (storageTime) {
+  time = parseInt(storageTime, 10);
+};
+
+
+var isTimerRunning = localStorage.getItem("isTimerRunning");
+if (isTimerRunning == "true") {
+  start_timer();
+};
+
+function start_timer() {
+    timer = setInterval(function() {
+    time--;
+
+    if (time == 0) {
+      spin_wheel();
+      clearInterval(timer);
+      time = 10;
+    };
+    updateTimer();
+  }, 1000)
+}
+
+
+function updateTimer() {
+  var minutes = Math.floor(time / 60);
+  var seconds = time % 60;
+
+  var formatedtime = (minutes < 10 ? "0": "") + minutes + ":" + (seconds < 10 ? "0": "") + seconds;
+
+  displayTimer.textContent = formatedtime;
+
+  localStorage.setItem("timerTime", time.toString());
+};
+
+window.addEventListener("beforeunload", function() {
+  localStorage.setItem("isTimerRunning", timer ? "true" : "false");
+})
+
+
 const wheel = document.getElementById("roulette-wheel");
 
 
 function spin_wheel() {
   if (wheel.classList.contains("roulette-wheel-spin")) {} else {
     wheel.classList.add("roulette-wheel-spin");
+    wheel_spinning = true;
     setTimeout(stop_spin_wheel, 4000);
   };
 };
@@ -37,6 +89,8 @@ function spin_wheel() {
 function stop_spin_wheel() {
   if (wheel.classList.contains("roulette-wheel-spin")) {
     wheel.classList.remove("roulette-wheel-spin");
+    wheel_spinning = false;
+    start_timer();
   };
 };
 
@@ -45,10 +99,11 @@ function stop_spin_wheel() {
 function place_coin(placeID) {
   var place = document.getElementById(placeID);
 
-  if (selected_coin) {
-    
+  if (selected_coin && !wheel_spinning) {
+    var generate_number = Math.floor(Math.random() * 37);
+    console.log(generate_number);
   } else {
-    alert("Válassz először egy tétet!")
+    alert("Jelenleg nem tehetsz tétet!");
   }
 }
 
